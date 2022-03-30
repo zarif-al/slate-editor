fc34c66b3a110e7e218a729d24a14605e080105c
zarif-al <zarif_al96@outlook.com>
2022-03-30 13:49:28 +0600

    prettier update

--- components/editor/render-element/heading-one.tsx
+++ components/editor/render-element/heading-one.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { RenderProps } from "@/utils/editor/types";
+import React from 'react';
+import { RenderProps } from '@/utils/editor/types';
 const HeadingOne = (props: RenderProps): JSX.Element => {
-	return <h1 {...props.attributes}>{props.children}</h1>;
+  return <h1 {...props.attributes}>{props.children}</h1>;
 };
 
 export default HeadingOne;
