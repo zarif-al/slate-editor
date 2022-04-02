17251e69e1dc1bc747e892b364154c7dd2903292
zarif-al <zarif_al96@outlook.com>
2022-04-02 13:21:13 +0600

    handle backspace from different block to empty list item

--- components/editor/plugins/withCleanLists.ts
+++ components/editor/plugins/withCleanLists.ts
@@ -5,7 +5,7 @@ import { ElementEnums } from '@/utils/editor/enums';
 // Normal behavior renders a list-item without a parent ordered/unordered list tag
 
 const withCleanLists = (editor: Editor): Editor => {
-  const { deleteBackward } = editor;
+  const { deleteBackward, deleteForward } = editor;
 
   editor.deleteBackward = (unit): void => {
     const { selection } = editor;
@@ -17,36 +17,47 @@ const withCleanLists = (editor: Editor): Editor => {
       Range.isCollapsed(selection)
     ) {
       const node = editor.children[selection.anchor.path[0]] as CustomElement | undefined;
+      const prevNode = editor.children[selection.anchor.path[0] - 1] as CustomElement | undefined;
+
+      // If the previous node is a lis
       if (
-        (node?.type === ElementEnums.NumberedList || node?.type === ElementEnums.BulletedList) &&
-        node.children.length === 1
+        prevNode &&
+        (prevNode.type === ElementEnums.NumberedList || prevNode.type === ElementEnums.BulletedList)
       ) {
-        // If the list is not on the first line
-        if (selection.anchor.path[0] > 0) {
-          Transforms.setNodes(
-            editor,
-            {
-              type: ElementEnums.Paragraph,
-              children: [{ text: '' }],
-            },
-            { at: [selection.anchor.path[0], 0] },
-          );
-        } else {
-          // If the list is on the first line
-          Transforms.unwrapNodes(editor, {
-            match: (n) =>
-              Element.isElement(n) &&
-              (n.type == ElementEnums.BulletedList || n.type == ElementEnums.NumberedList),
-            split: true,
-          });
-          Transforms.setNodes(
-            editor,
-            { type: ElementEnums.Paragraph, children: [{ text: '' }] },
-            { match: (n) => Editor.isBlock(editor, n), split: false },
-          );
+        Transforms.move(editor, { distance: 1, unit: 'character', reverse: true });
+        Transforms.removeNodes(editor, { at: [editor.children.length - 1] });
+      } else {
+        if (
+          (node?.type === ElementEnums.NumberedList || node?.type === ElementEnums.BulletedList) &&
+          node.children.length === 1
+        ) {
+          // If the list is not on the first line
+          if (selection.anchor.path[0] > 0) {
+            Transforms.setNodes(
+              editor,
+              {
+                type: ElementEnums.Paragraph,
+                children: [{ text: '' }],
+              },
+              { at: [selection.anchor.path[0], 0] },
+            );
+          } else {
+            // If the list is on the first line
+            Transforms.unwrapNodes(editor, {
+              match: (n) =>
+                Element.isElement(n) &&
+                (n.type == ElementEnums.BulletedList || n.type == ElementEnums.NumberedList),
+              split: true,
+            });
+            Transforms.setNodes(
+              editor,
+              { type: ElementEnums.Paragraph, children: [{ text: '' }] },
+              { match: (n) => Editor.isBlock(editor, n), split: false },
+            );
+          }
         }
+        deleteBackward(unit);
       }
-      deleteBackward(unit);
     } else {
       deleteBackward(unit);
     }
