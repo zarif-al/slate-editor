4c3de00623899b4c013f58f27b046b2e946ab4e5
zarif-al <zarif_al96@outlook.com>
2022-05-13 15:03:21 +0600

    added cors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'micro-cors';
 
 type Data = {
-  name: string
+  name: string;
+};
+
+const cors = Cors({
+  allowMethods: ['GET', 'HEAD'],
+  origin: 'https://vercel.com',
+});
+
+function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'John Doe' });
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default cors(handler as any);
